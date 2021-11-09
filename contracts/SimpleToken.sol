c9e9ebedab6d26857f27ca7f8065d2062ca26cfb
freddyaboulton <alfonsoboulton@gmail.com>
2021-11-08 22:38:59 -0500

    Add presale functionality + payable

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -12,14 +12,40 @@ contract SimpleToken is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     uint256 public constant TOTAL_SUPPLY = 10;
+    uint public constant PRESALE_SUPPLY = 5;
+    uint public constant SALE_SUPPLY = 5;
+    uint public constant SALE_PRICE = 1 wei;
+    mapping (address => bool) private presaleList;
+    bool public IS_PRESALE = false;
+    bool public IS_SALE = false;
     
     string private _tokenBaseURI = "https://my-json-server.typicode.com/freddyaboulton/pirata-token/tokens/";
 
-    
     constructor() ERC721("SimpleToken", "STKN") { }
     
+    function addToPresale(address[] calldata _addresses) external onlyOwner {
+        for(uint i = 0; i < _addresses.length; i++){
+            address account = _addresses[i];
+            require(account != address(0), "Invalid Address!");
+            require(!presaleList[account], "Already on presale list");
+            presaleList[account] = true;
+        }
+    }
+
+    function mintPresale(uint tokenQuantity) external payable {
+        require(msg.value >= SALE_PRICE * tokenQuantity, "NOT_ENOUGH_MONEY");
+        require(IS_PRESALE, "NOT PRESALE");
+        require(tokenQuantity > 0, "Invalid Token Quantity");
+        require(totalSupply() < PRESALE_SUPPLY, "OUT_OF_STOCK");
+        require(totalSupply() + tokenQuantity <= PRESALE_SUPPLY, "NOT_ENOUGH_IN_STOCK");
+        require(presaleList[msg.sender], "NOT ON PRESALE");
+        for(uint i = 0; i < tokenQuantity; i++){
+            _safeMint(msg.sender, totalSupply() + 1);
+        }
+    }
     
-    function mint(uint256 tokenQuantity) external {
+    function mint(uint256 tokenQuantity) external payable {
+        require(IS_SALE, "NOT SALE");
         require(totalSupply() < TOTAL_SUPPLY, "OUT_OF_STOCK");
         require(totalSupply() + tokenQuantity <= TOTAL_SUPPLY, "NOT_ENOUGH_IN_STOCK");
         
@@ -32,5 +58,13 @@ contract SimpleToken is ERC721Enumerable, Ownable {
         require(_exists(tokenId), "Cannot query non-existent token");
         
         return string(abi.encodePacked(_tokenBaseURI, tokenId.toString()));
+    }
+
+    function setPresale(bool value) external onlyOwner {
+        IS_PRESALE = value;
+    }
+
+    function setSale(bool value) external onlyOwner {
+        IS_SALE = value;
     }
 }
--- tests/test_simple_token.py
+++ tests/test_simple_token.py
@@ -13,35 +13,65 @@ def simple_token_contract(SimpleToken, accounts):
 def test_init(simple_token_contract, accounts):
     assert accounts[0].address == simple_token_contract.owner()
     assert simple_token_contract.TOTAL_SUPPLY() == 10
+    assert not simple_token_contract.IS_PRESALE()
+    assert not simple_token_contract.IS_SALE()
 
 
-def test_owner_can_mint(simple_token_contract, accounts):
-    simple_token_contract.mint(1, {'from': accounts[0]})
+def test_presale_mint(simple_token_contract, accounts):
+    simple_token_contract.setPresale(True, {'from': accounts[0]})
+
+    simple_token_contract.addToPresale([accounts[0]], {'from': accounts[0]})
+    simple_token_contract.mintPresale(1, {'from': accounts[0], 'value': "1 wei"})
     assert simple_token_contract.ownerOf(1) == accounts[0]
     assert simple_token_contract.balanceOf(accounts[0]) == 1
     assert simple_token_contract.totalSupply() == 1
     assert simple_token_contract.tokenURI(1) == "https://my-json-server.typicode.com/freddyaboulton/pirata-token/tokens/1"
+    assert simple_token_contract.balance() == "1 wei"
 
-def test_multiple_people_can_mint(simple_token_contract, accounts):
-    simple_token_contract.mint(1, {'from': accounts[1]})
-    simple_token_contract.mint(3, {'from': accounts[2]})
-    simple_token_contract.mint(4, {'from': accounts[3]})
+
+def test_cannot_pint_presale_if_not_on_list(simple_token_contract, accounts):
+    simple_token_contract.setPresale(True, {'from': accounts[0]})
+    with pytest.raises(VirtualMachineError, match="NOT ON PRESALE"):
+        simple_token_contract.mintPresale(1, {'from': accounts[1], 'value': "1 wei"})
+
+
+def test_multiple_people_can_mint_presale(simple_token_contract, accounts):
+    simple_token_contract.setPresale(True, {'from': accounts[0]})
+    simple_token_contract.addToPresale(accounts[:4], {'from': accounts[0]})
+
+    simple_token_contract.mintPresale(1, {'from': accounts[1], 'value': "1 wei"})
+    simple_token_contract.mintPresale(2, {'from': accounts[2], 'value': "2 wei"})
+    simple_token_contract.mintPresale(1, {'from': accounts[3], 'value': "1 wei"})
 
     assert simple_token_contract.balanceOf(accounts[1]) == 1
-    assert simple_token_contract.balanceOf(accounts[2]) == 3
-    assert simple_token_contract.balanceOf(accounts[3]) == 4
+    assert simple_token_contract.balanceOf(accounts[2]) == 2
+    assert simple_token_contract.balanceOf(accounts[3]) == 1
 
     assert simple_token_contract.ownerOf(1) == accounts[1]
     
-    for i in range(2, 5):
+    for i in range(2, 4):
         assert simple_token_contract.ownerOf(i) == accounts[2]
 
-    for i in range(5, 9):
-        assert simple_token_contract.ownerOf(i) == accounts[3]
+    assert simple_token_contract.ownerOf(4) == accounts[3]
+
 
 def test_cannot_buy_if_out_of_stock(simple_token_contract, accounts):
-    tx = simple_token_contract.mint(10, {'from': accounts[1]})
+    simple_token_contract.setPresale(True, {'from': accounts[0]})
+    simple_token_contract.addToPresale([accounts[1], accounts[3], accounts[5]], {'from': accounts[0]})
+    simple_token_contract.mintPresale(4, {'from': accounts[1], 'value': "4 wei"})
+    
+    with pytest.raises(VirtualMachineError, match="NOT_ENOUGH_IN_STOCK"):
+        simple_token_contract.mintPresale(2, {'from': accounts[3], 'value': "2 wei"})
 
-    with pytest.raises(Exception):
-        tx = simple_token_contract.mint(2, {'from': accounts[0]})
-        tx.wait(0.2)
+    simple_token_contract.mintPresale(1, {'from': accounts[5], 'value': '1 wei'})
+
+    with pytest.raises(VirtualMachineError, match="OUT_OF_STOCK"):
+        simple_token_contract.mintPresale(2, {'from': accounts[7], 'value': "2 wei"})
+
+
+def test_can_mint_during_sale(simple_token_contract, accounts):
+    simple_token_contract.setSale(True, {'from': accounts[0]})
+    simple_token_contract.mint(1, {'from': accounts[4], 'value': "1 wei"})
+    simple_token_contract.mint(2, {'from': accounts[5], 'value': "2 wei"})
+    assert simple_token_contract.balanceOf(accounts[4]) == 1
+    assert simple_token_contract.balanceOf(accounts[5]) == 2
